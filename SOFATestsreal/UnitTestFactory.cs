d9dcc3e19ea24786154e8b065bfc6e2055f67818
jeroenlooijtu <jeroen.looij2406@gmail.com>
2025-07-10 08:39:53 +0200

    yeerra

--- SOFATestsreal/UnitTestFactory.cs
+++ SOFATestsreal/UnitTestFactory.cs
@@ -25,4 +25,35 @@ public class UnitTestFactory : StateTestBase
         PngRapportage rapport = Factory.CreatePngRapportage(sprint, "Company name", "Product name");
         Assert.NotNull(rapport);
     }
+
+    [Fact]
+    public void ExecutePdfRapport()
+    {
+        //Arrange
+        Sprint sprint = new Sprint(new DateOnly(2025, 12, 25), new DateOnly(2026, 1, 23));
+        PdfRapportage rapport = Factory.CreatePdfRapportage(sprint, "Company name", "Product name");
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+        //Act
+        rapport.CreateRapport();
+        String x = sw.ToString().Replace("\r\n", "").Trim();
+        //Assert
+        Assert.Equal("PDF Rapport made", x);
+        
+    }
+
+    [Fact]
+    public void ExecutePngRapport()
+    {
+        //Arrange
+        Sprint sprint = new Sprint(new DateOnly(2025, 12, 25), new DateOnly(2026, 1, 23));
+        PngRapportage rapport = Factory.CreatePngRapportage(sprint, "Company name", "Product name");
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+        //Act
+        rapport.CreateRapport();
+        String x = sw.ToString().Replace("\r\n", "").Trim();
+        //Assert
+        Assert.Equal("Create PNG Rapport", x);
+    }
 }
