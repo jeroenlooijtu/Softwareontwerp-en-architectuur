0a1fa6247f235fc051ef1b034e581a6738e77548
jeroenlooijtu <jeroen.looij2406@gmail.com>
2025-07-10 08:01:48 +0200

    waha

---
+++ Softwareontwerp en architectuur/Domain/Factory/PDFRapportage.cs
@@ -0,0 +1,14 @@
+﻿namespace Softwareontwerp_en_architectuur.Domain.Factory;
+
+public class PdfRapportage : Rapportage
+{
+    public PdfRapportage(string footer, string header, Sprint sprint) : base(footer, header, sprint)
+    {
+    }
+
+
+    public override void CreateRapport()
+    {
+        Console.Write("PDF Rapport made");
+    }
+}
---
+++ Softwareontwerp en architectuur/Domain/Factory/RapportageFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Softwareontwerp_en_architectuur.Domain.Factory;
+
+public class RapportageFactory
+{
+    public RapportageFactory()
+    {
+        
+    }
+
+    public PngRapportage CreatePngRapportage(Sprint sprint, string header, string footer)
+    {
+        return new PngRapportage(footer, header, sprint);
+    }
+    
+    public PdfRapportage CreatePdfRapportage(Sprint sprint, string header, string footer)
+    {
+        return new PdfRapportage(footer, header, sprint);
+    }
+}
